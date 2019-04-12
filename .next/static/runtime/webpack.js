c914ae871084c417ce176fe3710ac3baba95ed90
Gabriel Omar Hinojoza Velarde <39060802+GaboFrontDev@users.noreply.github.com>
2019-04-11 20:18:06 -0500

    ajustes a estilos

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d1f430cf4817274a15fb";
+/******/ 	var hotCurrentHash = "a6c3957a29d57d19eeba";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
